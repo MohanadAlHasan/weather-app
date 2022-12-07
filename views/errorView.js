05b042972a074bf39605955c61a04dd2588fef4b
Mohanad Al Hasan <104508648+MohanadAlHasan@users.noreply.github.com>
2022-12-07 11:24:53 +0100

    Add files via upload

---
+++ views/errorView.js
@@ -0,0 +1,5 @@
+import { weatherApp } from "../src/constants.js";
+export const showError = () => {
+  weatherApp.innerHTML = "";
+  weatherApp.innerHTML = `<h2 id= error>Error page not found</h2>`;
+};
