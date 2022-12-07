d15bea814fab7d145769665d85066f9cffc70ec1
Mohanad Al Hasan <104508648+MohanadAlHasan@users.noreply.github.com>
2022-12-07 10:54:43 +0100

    Delete constants.js

--- constants.js
+++
@@ -1,5 +0,0 @@
-export const timeEl = document.querySelector(".time");
-export const dateEl = document.querySelector(".date");
-export const weatherTem = document.querySelector(".weather");
-export const details = document.querySelector(".details");
-export const weatherApp = document.querySelector(".weather-app");
