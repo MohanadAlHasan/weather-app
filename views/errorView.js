8de21efefc3275ca27e90d9ba17d7657bf30d23e
Mohanad Al Hasan <104508648+MohanadAlHasan@users.noreply.github.com>
2022-12-07 11:24:14 +0100

    Delete errorView.js

--- views/errorView.js
+++
@@ -1,5 +0,0 @@
-import { weatherApp } from "./src/constants.js";
-export const showError = () => {
-  weatherApp.innerHTML = "";
-  weatherApp.innerHTML = `<h2 id= error>Error page not found</h2>`;
-};
