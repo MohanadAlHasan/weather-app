1ccb9c403a076ad0dfc4e11889c4d16890316598
Mohanad Al Hasan <104508648+MohanadAlHasan@users.noreply.github.com>
2022-12-07 10:54:32 +0100

    Delete background.js

--- background.js
+++
@@ -1,9 +0,0 @@
-export const background = async (cityName) => {
-  const apiKy = `ASx8Vd2ZTLBzo9dDtTrt0_Fn8X5sdyupAC0B31SJxRU`;
-  const url = `https://api.unsplash.com/search/photos?client_id=${apiKy}&query=${cityName}`;
-  const response = await fetch(url);
-  const myData = await response.json();
-
-  const app = document.querySelector(".weather-app");
-  app.style.backgroundImage = `url(${myData.results[0].urls.regular})`;
-};
